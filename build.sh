511e25fe47dc9d73e687920738a28319d81d3b53
gekyxme <gxkyume.7@gmail.com>
2024-03-28 17:50:51 +0530

    fix: build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-# Create a virtual environment with Python 3.9
-python3.9.9 -m venv venv
-
-# Activate the virtual environment
+python3.9 -m venv venv
 source venv/bin/activate
-
-# Install dependencies from requirements.txt
 pip install -r requirements.txt
-
-# Since there's no manage.py, you can just start your Flask app directly
-python app.py
