2866d636f8b5f721283318e23499952e787fc347
gekyxme <gxkyume.7@gmail.com>
2024-04-30 22:06:18 +0530

    feat: exp regex

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 from flask import Flask, request, jsonify
 import joblib
+import re
+
+# Regular expression pattern to match expenses in the format "<amount> Rs" or "Rs <amount>"
+expense_pattern = r'(?i)(?:Rs\.?|rs\.?)? (\d+(?:\.\d+)?)|(?:\d+(?:\.\d+)?) (?i)(?:Rs\.?|rs\.?)'
+
+
+# Function to extract expenses from input text
+def extract_expense(input_text):
+    match = re.search(expense_pattern, input_text)
+    if match:
+        # Find the matched group that contains the expense amount
+        for group in match.groups():
+            if group:
+                return float(group)
+    return None
 
 app = Flask(__name__)
 
@@ -19,10 +34,13 @@ def classify_text():
     # Preprocess and vectorize input text
     text_vector = vectorizer.transform([input_text])
     
+    # Extract Expense
+    extracted_exp = extract_expense(input_text)
+
     # Classify input text
     predicted_label = classifier.predict(text_vector)[0]
     
-    return jsonify({'predicted_label': predicted_label})
+    return jsonify({'predicted_label': predicted_label, 'expense': extracted_exp})
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
